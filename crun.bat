76a726d2cfae031c850d9cd4cd68dad7a4566f1e
Adriel de Souza <ds.adriel@gmail.com>
2025-05-03 20:05:24 -0300

    reorganize project structure

--- crun.bat
+++ crun.bat
@@ -1,6 +1,6 @@
-rem script para MONTAR, LIGAR e executar o programa
-rem USO: crun <nomeDoPrograma>
-rem Não inclua a extensão do arquivo, ex.: se o arquivo se chama trabalho.asm use 'crun trabalho'
+rem script to ASSEMBLE, LINK, and execute the program
+rem USAGE: crun <programName>
+rem Do not include the file extension, e.g.: if the file is named trabalho.asm use 'crun trabalho'
 rescan
 masm /Zi %1.asm,%1.obj,%1.lst,%1.crf;
 link /CO %1.obj,,,,;
